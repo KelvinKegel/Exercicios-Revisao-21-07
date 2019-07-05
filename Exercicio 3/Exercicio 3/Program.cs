4484e53c5df30fef1b3b083f1c887706e655b0b0
KelvinKegel <kelvinkegel@gmail.com>
2019-07-05 12:32:06 -0300

    gg

--- Exercicio 3/Exercicio 3/Program.cs
+++ Exercicio 3/Exercicio 3/Program.cs
@@ -10,6 +10,50 @@ namespace Exercicio_3
     {
         static void Main(string[] args)
         {
+            List<double> gold = new List<double>();
+            List<double> montarias = new List<double>();
+
+            int input = -1;
+
+            Console.WriteLine("Digite um número negativo quando desejar parar.");
+            Console.WriteLine("Digite a quantidade de gold, dê enter, e digite a quantidade de montarias:");
+
+            while (input != 0)
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input > 0)
+                    gold.Add(input);
+                else if (input < 0)
+                {
+                    Console.WriteLine("Por ser um número negativo, deu erro");
+                    break;
+                }
+
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input > 0)
+                    montarias.Add(input);
+                else if (input < 0)
+                {
+                    Console.WriteLine("Por ser um número negativo, deu erro");
+                    break;
+                }
+            }
+            Console.WriteLine("O número abaixo é a média de gold dos jogadores:");
+            Console.WriteLine(gold.Average());
+
+            Console.WriteLine("O número abaixo é a média de montarias dos jogadores:");
+            Console.WriteLine(montarias.Average());
+
+            Console.WriteLine("O número abaixo é a quantidade maior de gold que um jogador possui:");
+            gold.Sort();
+            Console.WriteLine(gold.Last());
+
+            Console.WriteLine("Este é o percentual de jogadores com até 100 gold:");
+            Console.WriteLine(((double)gold.FindAll(x => x <= 100).Count / (double)gold.Count) * 100 + "%");
+
+            Console.ReadKey();
         }
     }
 }
