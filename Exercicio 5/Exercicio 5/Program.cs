e0c417d0d0b514543804a97641d10faa64902a60
KelvinKegel <kelvinkegel@gmail.com>
2019-07-09 12:21:44 -0300

    .

--- Exercicio 5/Exercicio 5/Program.cs
+++ Exercicio 5/Exercicio 5/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Exercicio_5
 {
+    class Personagem
+    {
+        public enum Cores
+        {
+            Azul,
+            Castanho,
+            Verde,
+            Louro,
+            Preto
+        }
+        public Cores olhos;
+        public Cores cabelo;
+        public bool homem;
+        public int level;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List<Personagem> listaDeDados = new List<Personagem>();
+
+            int level = 1;
+            while (level >= 0)
+            {
+                Personagem cobaia = new Personagem();
+                string sexo =  Console.ReadLine();
+                cobaia.homem = (sexo == "m");
+                cobaia.olhos = Personagem.Cores.Azul;
+                listaDeDados.Add(cobaia);
+            }
+
         }
     }
 }
